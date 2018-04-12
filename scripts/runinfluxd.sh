46e8f40ceba1c9c63b2267c73073ed130b0ddde1
Ian Johnson <ian.johnson@canonical.com>
2018-04-12 13:55:54 -0500

    change the meta, data, and wal directories to be in $SNAP_DATA
    The default location of /var/lib/influxdb isn't writable on Ubuntu-Core, so this was preventing the server from running.
    Also, putting the config file into conf rather than in the root of $SNAP_DATA

--- scripts/runinfluxd.sh
+++ scripts/runinfluxd.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 set -e
- 
-$SNAP/bin/influxd -config $SNAP_DATA/influxdb.conf
+
+export INFLUXDB_META_DIR=$SNAP_DATA/meta
+export INFLUXDB_DATA_DIR=$SNAP_DATA/data
+export INFLUXDB_DATA_WAL_DIR=$SNAP_DATA/wal
+$SNAP/bin/influxd -config $SNAP_DATA/conf/influxdb.conf
