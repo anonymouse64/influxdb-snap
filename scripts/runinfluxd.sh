c16720f2c2ee7b4acd69bf4afb299cd689f3e43c
Ian Johnson <ian.johnson@canonical.com>
2020-10-29 16:07:22 -0500

    snapcraft.yaml: rm wrapper, apply yaml linter, + use environment for daemon
    Signed-off-by: Ian Johnson <ian.johnson@canonical.com>

--- scripts/runinfluxd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-
-export INFLUXDB_META_DIR=$SNAP_DATA/meta
-export INFLUXDB_DATA_DIR=$SNAP_DATA/data
-export INFLUXDB_DATA_WAL_DIR=$SNAP_DATA/wal
-$SNAP/bin/influxd -config $SNAP_DATA/conf/influxdb.conf
